ea5047d9032ff2bae77f7e427b3b705515d7beea
HasaraR <hasara.randunu@gmail.com>
2025-03-04 20:40:01 +0530

    pharmacy done

--- src/app/pharmacy/page.tsx
+++ src/app/pharmacy/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
 
+import Pharmacy from "../components/Pharmacy";
+
 export default function Home() {
   return (
-    <div>
-      
+    <div >
+      <Pharmacy />
+    
     </div>
   );
 }
